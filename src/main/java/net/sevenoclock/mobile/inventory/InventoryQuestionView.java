2ad3c780aeb077ea6e78e208db6f56d12929286b
JerryPark <eaeao@naver.com>
2015-09-15 22:08:01 +0900

    대량 문제 로드 이미지 로드 실패

--- src/main/java/net/sevenoclock/mobile/inventory/InventoryQuestionView.java
+++ src/main/java/net/sevenoclock/mobile/inventory/InventoryQuestionView.java
@@ -1,38 +1,54 @@
 package net.sevenoclock.mobile.inventory;
 
 import android.content.Context;
-import android.util.DisplayMetrics;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import com.androidquery.AQuery;
+import android.widget.Toast;
+import com.androidquery.callback.AjaxCallback;
+import com.androidquery.callback.AjaxStatus;
 import net.sevenoclock.mobile.R;
 import net.sevenoclock.mobile.customobj.TryCatchJO;
 import net.sevenoclock.mobile.settings.Functions;
 import net.sevenoclock.mobile.settings.Values;
+
+import java.io.File;
 
 public class InventoryQuestionView extends LinearLayout {
 
     public InventoryQuestionView(Context context, TryCatchJO jo) {
         super(context);
         try{
-            Values valeus = (Values)context.getApplicationContext();
+            final Values valeus = (Values)context.getApplicationContext();
             LayoutParams lp = new LayoutParams(LayoutParams.MATCH_PARENT, valeus.book_height * 2 - 100);
             lp.setMargins(7, 7, 7, 7);
             setLayoutParams(lp);
             setOrientation(VERTICAL);
 
-            ImageView iv_inventory_question_list_question_img = new ImageView(context);
+            final ImageView iv_inventory_question_list_question_img = new ImageView(context);
 
-            valeus.aq.id(iv_inventory_question_list_question_img).image(Functions.DOMAIN + jo.get("src", ""));
             iv_inventory_question_list_question_img.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
             iv_inventory_question_list_question_img.setScaleType(ImageView.ScaleType.FIT_START);
             iv_inventory_question_list_question_img.setBackgroundResource(R.drawable.ll_inventory_question_list_question);
 
+            valeus.aq.ajax(Functions.DOMAIN + jo.get("src", ""), File.class, new AjaxCallback<File>() {
+                public void callback(String url, File file, AjaxStatus status) {
+                    if (file != null) {
+                        BitmapFactory.Options opts = new BitmapFactory.Options();
+                        opts.inSampleSize = 2;
+                        Bitmap buttonImages = BitmapFactory.decodeFile(file.getPath(), opts);
+                        valeus.aq.id(iv_inventory_question_list_question_img).image(buttonImages);
+                    } else {
+                        Toast.makeText(getContext(), "이미지 로드에 실패하였습니다.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
             addView(iv_inventory_question_list_question_img);
         }catch (Exception e){
-            Log.i("SearchQuestionError", e.getMessage());
+            Log.i("SearchQuestionError", ""+e.getMessage());
         }
     }
 }
--- src/main/java/net/sevenoclock/mobile/testpaper/TestpaperQuestionView.java
+++ src/main/java/net/sevenoclock/mobile/testpaper/TestpaperQuestionView.java
@@ -1,43 +1,60 @@
 package net.sevenoclock.mobile.testpaper;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.util.Log;
-import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import com.androidquery.AQuery;
+import android.widget.Toast;
+import com.androidquery.callback.AjaxCallback;
+import com.androidquery.callback.AjaxStatus;
 import net.sevenoclock.mobile.R;
 import net.sevenoclock.mobile.customobj.FontTextView;
 import net.sevenoclock.mobile.customobj.TryCatchJO;
 import net.sevenoclock.mobile.settings.Functions;
 import net.sevenoclock.mobile.settings.Values;
 
+import java.io.File;
+
 public class TestpaperQuestionView extends LinearLayout {
 
-    public TestpaperQuestionView(Context context, int index, TryCatchJO jo) {
+    public TestpaperQuestionView(final Context context, int index, TryCatchJO jo) {
         super(context);
         try{
-            Values valeus = (Values)context.getApplicationContext();
+            final Values valeus = (Values)context.getApplicationContext();
             LayoutParams lp = new LayoutParams(LayoutParams.MATCH_PARENT, valeus.book_height * 2 - 100);
             lp.setMargins(7, 7, 7, 7);
             setLayoutParams(lp);
             setOrientation(VERTICAL);
 
             FontTextView tv_testpaper_question_list_question_number = new FontTextView(context);
-            ImageView iv_testpaper_question_list_question_img = new ImageView(context);
+            final ImageView iv_testpaper_question_list_question_img = new ImageView(context);
 
-            tv_testpaper_question_list_question_number.setText((index +1)+".");
+            tv_testpaper_question_list_question_number.setText((index + 1) + ".");
             tv_testpaper_question_list_question_number.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
 
-            valeus.aq.id(iv_testpaper_question_list_question_img).image(Functions.DOMAIN + jo.get("src_url", ""));
             iv_testpaper_question_list_question_img.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
             iv_testpaper_question_list_question_img.setScaleType(ImageView.ScaleType.FIT_START);
             iv_testpaper_question_list_question_img.setBackgroundResource(R.drawable.ll_testpaper_question_list_question);
 
+            valeus.aq.ajax(Functions.DOMAIN + jo.get("src_url", ""), File.class, new AjaxCallback<File>() {
+                public void callback(String url, File file, AjaxStatus status) {
+                    if (file != null) {
+                        BitmapFactory.Options opts = new BitmapFactory.Options();
+                        opts.inSampleSize = 2;
+                        Bitmap buttonImages = BitmapFactory.decodeFile(file.getPath(), opts);
+                        valeus.aq.id(iv_testpaper_question_list_question_img).image(buttonImages);
+                    } else {
+                        Toast.makeText(getContext(), "이미지 로드에 실패하였습니다.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
             addView(tv_testpaper_question_list_question_number);
             addView(iv_testpaper_question_list_question_img);
         }catch (Exception e){
-            Log.i("TestpaperError", e.getMessage());
+            Log.i("TestpaperError", ""+e.getMessage());
         }
     }
 }
