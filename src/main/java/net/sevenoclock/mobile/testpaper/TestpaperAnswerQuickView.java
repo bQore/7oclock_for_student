93957c28c6acab3f440ac954d2935e36af2add48
JerryPark <eaeao@naver.com>
2015-10-13 20:19:29 +0900

    최적화

--- src/main/java/net/sevenoclock/mobile/testpaper/TestpaperAnswerQuickView.java
+++ src/main/java/net/sevenoclock/mobile/testpaper/TestpaperAnswerQuickView.java
@@ -1,12 +1,14 @@
 package net.sevenoclock.mobile.testpaper;
 
 import android.content.Context;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import net.sevenoclock.mobile.R;
 import net.sevenoclock.mobile.customobj.TryCatchJO;
+import net.sevenoclock.mobile.main.MainActivity;
 import net.sevenoclock.mobile.question.QuestionFragmentView;
 import net.sevenoclock.mobile.settings.Functions;
 import net.sevenoclock.mobile.settings.Values;
@@ -24,7 +26,7 @@ public class TestpaperAnswerQuickView extends LinearLayout {
 
     Values values;
 
-    public TestpaperAnswerQuickView(Context context, JSONArray ja_question) {
+    public TestpaperAnswerQuickView(Context context, final JSONArray ja_question) {
         super(context);
         this.con = context;
         this.ja_question = ja_question;
@@ -40,21 +42,30 @@ public class TestpaperAnswerQuickView extends LinearLayout {
         setTag(R.string.tag_main_title, "빠른 정답 보기");
         setTag(R.string.tag_main_subtitle, "");
 
-        tarqv = new TestpaperAnswerResultQuickView[ja_question.length()];
-        for (int i = 0; i < ja_question.length(); i++) {
-            try {
-                TryCatchJO tcjo_question = new TryCatchJO(ja_question.getJSONObject(i));
-                tarqv[i] = new TestpaperAnswerResultQuickView(con, i, tcjo_question);
-                tarqv[i].setOnClickListener(new OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Functions.history_go(con, new QuestionFragmentView(con, ((TestpaperAnswerResultQuickView) v).tcjo_info));
+        MainActivity.ll_main_main_loading.setVisibility(View.VISIBLE);
+        Handler handler = new Handler();
+        Runnable r = new Runnable() {
+            public void run() {
+                tarqv = new TestpaperAnswerResultQuickView[ja_question.length()];
+                for (int i = 0; i < ja_question.length(); i++) {
+                    try {
+                        TryCatchJO tcjo_question = new TryCatchJO(ja_question.getJSONObject(i));
+                        tarqv[i] = new TestpaperAnswerResultQuickView(con, i, tcjo_question);
+                        tarqv[i].setOnClickListener(new OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Functions.history_go(con, new QuestionFragmentView(con, ((TestpaperAnswerResultQuickView) v).tcjo_info));
+                            }
+                        });
+                        ll_testpaper_input_quick_forms.addView(tarqv[i]);
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
-                });
-                ll_testpaper_input_quick_forms.addView(tarqv[i]);
-            } catch (Exception e) {
-                e.printStackTrace();
+                }
+                MainActivity.ll_main_main_loading.setVisibility(View.GONE);
             }
-        }
+        };
+        handler.postDelayed(r, 1);
+
     }
 }
--- src/main/java/net/sevenoclock/mobile/testpaper/TestpaperAnswerResultQuickView.java
+++ src/main/java/net/sevenoclock/mobile/testpaper/TestpaperAnswerResultQuickView.java
@@ -3,7 +3,10 @@ package net.sevenoclock.mobile.testpaper;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import net.sevenoclock.mobile.R;
 import net.sevenoclock.mobile.customobj.FontTextView;
@@ -19,11 +22,6 @@ public class TestpaperAnswerResultQuickView extends LinearLayout {
     public int qid = 0;
     public String answer = "";
 
-    FontTextView tv_testpaper_answer_result_quick_index;
-    FontTextView tv_testpaper_answer_result_quick_answer;
-
-    Values values;
-
     public TestpaperAnswerResultQuickView(Context context, int index, TryCatchJO jo) {
         super(context);
         this.con = context;
@@ -32,13 +30,36 @@ public class TestpaperAnswerResultQuickView extends LinearLayout {
         qid = tcjo_info.get("id",0);
         answer = tcjo_info.get("answer_mobile","");
 
-        values = (Values) context.getApplicationContext();
-        inflate(getContext(),R.layout.view_testpaper_answer_result_form, this);
+        int den = (int)(1*con.getResources().getDisplayMetrics().scaledDensity);
+
+        setBackgroundResource(R.drawable.ll_testpaper_answer_result_quick_line);
+        setGravity(Gravity.CENTER_VERTICAL);
         setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+        setOrientation(HORIZONTAL);
 
-        tv_testpaper_answer_result_quick_index = (FontTextView)findViewById(R.id.tv_testpaper_answer_result_quick_index);
+        FontTextView tv_testpaper_answer_result_quick_index = new FontTextView(con);
+        tv_testpaper_answer_result_quick_index.setText("1");
+        tv_testpaper_answer_result_quick_index.setGravity(Gravity.CENTER_VERTICAL|Gravity.CENTER_HORIZONTAL);
+        LayoutParams layout_703 = new LayoutParams(50*den,50*den);
+        layout_703.weight = 0;
+        tv_testpaper_answer_result_quick_index.setLayoutParams(layout_703);
+        addView(tv_testpaper_answer_result_quick_index);
 
-        tv_testpaper_answer_result_quick_answer = (FontTextView)findViewById(R.id.tv_testpaper_answer_result_quick_answer);
+        LinearLayout linearLayout_780 = new LinearLayout(con);
+        linearLayout_780.setBackgroundColor(Color.parseColor("#DDDDDD"));
+        LayoutParams layout_780 = new LayoutParams(1*den,LayoutParams.MATCH_PARENT);
+        layout_780.weight = 0;
+        linearLayout_780.setLayoutParams(layout_780);
+        addView(linearLayout_780);
+
+        FontTextView tv_testpaper_answer_result_quick_answer = new FontTextView(con);
+        tv_testpaper_answer_result_quick_answer.setText("New Text");
+        tv_testpaper_answer_result_quick_answer.setTextSize(16);
+        tv_testpaper_answer_result_quick_answer.setTypeface(Typeface.DEFAULT_BOLD);
+        tv_testpaper_answer_result_quick_answer.setGravity(Gravity.CENTER_VERTICAL|Gravity.CENTER_HORIZONTAL);
+        LayoutParams layout_360 = new LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.MATCH_PARENT);
+        tv_testpaper_answer_result_quick_answer.setLayoutParams(layout_360);
+        addView(tv_testpaper_answer_result_quick_answer);
 
         if(qid != 0){
             tv_testpaper_answer_result_quick_index.setText("" + (index + 1));
